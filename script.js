aab33abfc6df9c4836ef8baa0f9f9cbc262e14d2
Lucas Lopes <68665689+lucaslopesx@users.noreply.github.com>
2021-09-13 17:19:45 -0300

    Commit inicial

---
+++ script.js
@@ -0,0 +1,6 @@
+function openPopup() {
+    document.getElementById("openPopup").style.display = "inline-block";
+}
+function closePopup() {
+    document.getElementById("openPopup").style.display = "none";
+}
