7784fa67b605cd03db5e5400ca668ee2aee1c961
Lucas Lopes <68665689+lucaslopesx@users.noreply.github.com>
2021-09-13 21:06:29 -0300

    Update with some functions and popup

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ function openPopup() {
 function closePopup() {
     document.getElementById("openPopup").style.display = "none";
 }
+
+function addProduct() {
+    let productName = document.getElementById("name").value;
+    let productDesc = document.getElementById("desc").value;
+    console.log(productName);
+    closePopup();
+}
